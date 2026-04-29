7fcace2b8b0b17149ba2893aeb6db56ef828a189
JoaoVitat <joaovitortavares012@gmail.com>
2026-04-29 19:15:37 -0300

    subindo projeto
    Subindo projeto inicial do trabalho

---
+++ VendinhaTrabalho/Models/Clientes.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace VendinhaTrabalho.Models
+{
+	public class Clientes
+	{
+		[Required(ErrorMessage = "O nome do aluno é obrigatório")]
+		[StringLength(100, MinimumLength = 10)]
+		[RegularExpression("^[A-Z][A-zA-z]+ [A-Z][A-zA-z ]+[^ ]$")]
+
+		public string Nome { get; set; }
+
+		[Required, StringLength(11)]
+		[RegularExpression("[0-9]+$")]
+		public string Cpf { get; set; }
+
+		public DateTime DataNascimento { get; set; }
+
+		[Range (15, 99)]
+		public int Idade
+		{
+			get 
+			{
+				var hoje = DateTime.Today;
+				var anos = hoje.Year - DataNascimento.Year;
+				var diaAnoNascimento = hoje.AddYears(-anos);
+				if (DataNascimento > diaAnoNascimento)
+				{
+					anos--;
+				}
+				return anos;
+			}
+		}
+	}
+}
---
+++ VendinhaTrabalho/Program.cs
@@ -0,0 +1,15 @@
+﻿while (true) {
+	Console.ReadKey();
+	Console.Clear();
+	Console.WriteLine("Escolha uma Opção:");
+
+	int opcao;
+	try {
+		opcao = int.Parse(Console.ReadLine());
+	}
+
+	if (opcao == 1)
+	{
+		Console.WriteLine("")
+	}
+}
